86844817ac05c246ba89498744451371292e0135
galang48 <galangyudha1945@gmail.com>
2024-06-13 11:41:47 +0700

    menambahkan method inorder preorder dan postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -45,12 +45,88 @@ public:
             return; // Exit
         }
 
-        
+        if (element < parent->info) //if the value is the data fielf of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // Mkae the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // If the value in the data field of the new node is greater than that 
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
+    }
+
+
+    void search(string element, Node*& parent, Node*& currentNode) 
+    {
+        //this function seacrhes the currentNode of the specified Node as well as the current node of its parent
+
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+    void inorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
+    void preorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    void postorder(Node* ptr)
+    {
+        //performs the postorder traversal of the tree
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
     }
 };
+    
 int main()
 {
-    
+    BinaryTree x;
+    while (true) 
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+    }
 }
 
 
