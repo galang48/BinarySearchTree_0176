8fe79c69d50d615fd0f9e12ce040bca456c6220f
galang48 <galangyudha1945@gmail.com>
2024-06-13 11:10:58 +0700

    membuat method insert

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -34,6 +34,10 @@ public:
         newNode->info = element; // Assign value to the data field of the new node
         newNode->leftchild = nullptr; // make the left child of the new node point to null
         newNode->rightchild = nullptr; //make the right child of the new node point to null
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be ins
     }
 };
 int main()
